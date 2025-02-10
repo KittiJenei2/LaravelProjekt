65bb04b77c098d9d6673ed8620a73da85a3983bc
Kitti <152151102+KittiJenei2@users.noreply.github.com>
2025-02-10 09:34:05 +0100

    02.10.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ Route::get('/makers', [MakerController::class, 'index'])->name('makers.index');
 //route-ok létrehozása
 Route::resource('makers', MakerController::class);
 Route::get('/makers/create', [MakerController::class, 'create'])->name('makers.create');
-Route::patch('/makers/{body}', [MakerController::class, 'update'])->name('makers.update');
+Route::patch('/makers/{maker}', [MakerController::class, 'update'])->name('makers.update');
+Route::delete('/makers/{maker}', [MakerController::class, 'destroy'])->name('makers.destroy');
 //Route::resource('fuels', FuelController::class);
 //Route::resource('models', ModelController::class);
 //Route::resource('bodies', BodyController::class);
